21f2ba5ff8a01ea40514d2adafb8c66a87f4541a
Jérémy Grosjean <jeremy@Jeremys-MacBook-Pro.local>
2026-05-02 17:48:37 +0200

    add deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'https://jbigjean.github.io',
+  base: '/acheter-louer',
+  integrations: [react()],
 });
